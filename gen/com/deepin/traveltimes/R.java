a5b3652462a557b20e3b9e89e40e39e9e50e4447
bluth653426595 <ycl_pj@163.com>
2013-07-20 16:16:05 +0800

    init timesline

--- gen/com/deepin/traveltimes/R.java
+++ gen/com/deepin/traveltimes/R.java
@@ -23,13 +23,18 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int imgtest1=0x7f020001;
+        public static final int imgtest2=0x7f020002;
+        public static final int imgtest3=0x7f020003;
     }
     public static final class id {
         public static final int bmapsView=0x7f070002;
         public static final int dummy_button=0x7f070003;
         public static final int fullscreen_content=0x7f070000;
         public static final int fullscreen_content_controls=0x7f070001;
-        public static final int listView1=0x7f070004;
+        public static final int img=0x7f070004;
+        public static final int info=0x7f070006;
+        public static final int title=0x7f070005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/deepin/traveltimes/TimesLine.java
+++ src/com/deepin/traveltimes/TimesLine.java
@@ -1,16 +1,51 @@
 package com.deepin.traveltimes;
 
-import android.app.Activity;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.SimpleAdapter;
 
-public class TimesLine extends Activity {
-
+/**
+ * @author ycl
+ * 
+ */
+public class TimesLine extends ListActivity {
+	// private List<String> data = new ArrayList<String>();
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.timesline);
 
+		SimpleAdapter adapter = new SimpleAdapter(this,getData(),R.layout.timesline,
+				new String[]{"title","info","img"},
+				new int[]{R.id.title,R.id.info,R.id.img});
+		setListAdapter(adapter);
 	}
 
+	private List<Map<String, Object>> getData() {
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("title", "G1");
+		map.put("info", "imgtest1");
+		map.put("img", R.drawable.imgtest1);
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+		map.put("title", "G2");
+		map.put("info", "imgtest2");
+		map.put("img", R.drawable.imgtest2);
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+		map.put("title", "G3");
+		map.put("info", "imgtest3");
+		map.put("img", R.drawable.imgtest3);
+		list.add(map);
+		
+		return list;
+	}
 }
