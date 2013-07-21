b19a9be2e29a4bd84eab4367771686b2abef03f2
bluth653426595 <ycl_pj@163.com>
2013-07-21 08:25:35 +0800

    add login

--- src/com/deepin/traveltimes/MainActivity.java
+++ src/com/deepin/traveltimes/MainActivity.java
@@ -1,7 +1,9 @@
 package com.deepin.traveltimes;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends Activity {
 
@@ -9,7 +11,22 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		handler.postDelayed(runnable, 500);// 每0.5秒执行一次runnable.
 	}
+
+	Handler handler = new Handler();
+	Runnable runnable = new Runnable() {
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			// 要做的事情
+			handler.postDelayed(this, 500);
+			Intent intent = new Intent(MainActivity.this, TimesLine.class);
+			startActivity(intent);
+			handler.removeCallbacks(runnable);
+			MainActivity.this.finish();
+
+		}
+	};
 
 }
