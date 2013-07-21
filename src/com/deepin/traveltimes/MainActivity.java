67fe7423af12af80b57f0caaae68eac789789790
iceleaf916 <kaisheng.ye@gmail.com>
2013-07-21 11:50:44 +0800

    update

--- src/com/deepin/traveltimes/MainActivity.java
+++ src/com/deepin/traveltimes/MainActivity.java
@@ -1,19 +1,5 @@
 package com.deepin.traveltimes;
- 
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.os.Bundle;
-import android.os.Handler;
-import android.webkit.WebView;
- 
-public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-	WebView wv;
-	ProgressDialog pd;
-	Handler handler;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
+public class MainActivity {
+
 }
